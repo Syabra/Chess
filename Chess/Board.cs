e3dd994ec84f3343358e298458b8946f079d2ee4
Yauheny <Syabra.bel@gmail.com>
2019-07-01 23:25:54 +0300

    Programming move figurs

--- Chess/Board.cs
+++ Chess/Board.cs
@@ -8,10 +8,11 @@ namespace Chess
 {
     class Board
     {
-        public string fen { get; private set; }
-        Figure[,] figures;
+        public string fen { get; private set; }        
         public Color moveColor { get; private set; }
         public int moveNumber { get; private set; }
+
+        Figure[,] figures;
 
         public Board (string fen)
         {
@@ -32,17 +33,44 @@ namespace Chess
             
         }
 
+        void GenerateFen()
+        {
+            fen = FenFigures() + " " +
+                   (moveColor == Color.white ? "w" : "b") 
+                   + " - - 0 " + moveNumber.ToString();
+        }
+
+        string FenFigures()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int y = 7; y >= 0; y--)
+            {
+                for (int x = 0; x < 8; x++)
+                    sb.Append(figures[x, y] == Figure.none ? '1' : (char)figures[x, y]);
+                if (y > 0)
+                    sb.Append('/');
+            }
+            string eight = "11111111";
+            for (int j = 8; j >= 2; j--)
+                sb.Replace(eight.Substring(0, j), j.ToString());
+
+            return sb.ToString();
+        }
+
         void InitFigures (string data) //placement of figures
         {
             for (int j = 8; j >= 2; j--)
                 data = data.Replace(j.ToString(), (j - 1).ToString() + "1");
+            data = data.Replace("1", ".");
             string[] lines = data.Split('/');
             for (int y = 7; y >= 0; y--)
                 for (int x = 0; x < 8; x++)
-                    figures[x, y] = (Figure)lines[7 - y][x];
+                    figures[x, y] = lines[7 - y][x] == '.' ? Figure.none : 
+                            (Figure)lines[7 - y][x];
 
         }
 
+        
         public Figure GetFigureAt (Square square)
         {
             if (square.OnBoard())
@@ -56,7 +84,7 @@ namespace Chess
                 figures[square.x, square.y] = figure;
         }
 
-        public Board Move (FigureMoving fm)
+        public Board Move (FigureMoving fm)  
         {
             Board next = new Board(fen);
             next.SetFigureAt(fm.from, Figure.none);
@@ -64,6 +92,7 @@ namespace Chess
             if (moveColor == Color.black)
                 next.moveNumber++;
             next.moveColor = moveColor.FlipColor();
+            next.GenerateFen();
             return next;
 
         }
--- Chess/FigureMoving.cs
+++ Chess/FigureMoving.cs
@@ -14,7 +14,7 @@ namespace Chess
         public Figure promotion { get; private set; }
 
 
-        public FigureMoving (FigureOnSquare fs, Square to, Figure promotion = Figure.none)
+        public FigureMoving(FigureOnSquare fs, Square to, Figure promotion = Figure.none)
         {
             figure = fs.figure;
             from = fs.square;
@@ -22,13 +22,26 @@ namespace Chess
             this.promotion = promotion;
         }
 
-        public FigureMoving (string move) //Pe2e4 Pe7e8Q
-                                          //01234 012345  
+        public FigureMoving(string move) //Pe2e4 Pe7e8Q
+                                         //01234 012345  
         {
             figure = (Figure)move[0];
             from = new Square(move.Substring(1, 2));
             to = new Square(move.Substring(3, 2));
             promotion = (move.Length == 6) ? (Figure)move[5] : Figure.none;
         }
+
+
+        public int DeltaX { get { return to.x - from.x; } }
+        public int DeltaY { get { return to.y - from.y; } }
+
+
+        public int AbsDeltaX { get { return Math.Abs(DeltaX); } }
+        public int AbsDeltaY { get { return Math.Abs(DeltaY); } }
+
+
+
+        public int SignX { get { return Math.Sign(DeltaX); } }
+        public int SignY { get { return Math.Sign(DeltaY); } }
     }
 }
